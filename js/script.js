1a11a8efeba8ab3ee967cdd6fa4eee79ecb3a49c
Milan Sony <milansonyofficial@gmail.com>
2024-03-20 09:27:48 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-window.onload=function(){
+window.onload = function () {
     // preloader
-    document.getElementById("loader").style.display="none";
-    document.getElementById("content").style.display="block";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").style.display = "block";
 };
 
-function DayNight(){
+function DayNight() {
     icon = document.getElementById("day-night")
 
     document.body.classList.toggle("dark-theme")
-    
-    if(document.body.classList.contains("dark-theme")){
+
+    if (document.body.classList.contains("dark-theme")) {
         icon.innerHTML = "🌙";
-    }else{
+    } else {
         icon.innerHTML = "☀️";
     }
 }
 
 // scroll to top btn
 let ScrollToTop = document.querySelector(".scroll-btn");
-window.addEventListener("scroll", () =>{
-    if(window.scrollY > 100){
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
         ScrollToTop.style.display = "flex";
-    }else{
+    } else {
         ScrollToTop.style.display = "none";
     }
 })
@@ -30,15 +30,15 @@ window.addEventListener("scroll", () =>{
 let toDaySpan = document.getElementById("today")
 let todayObj = new Date()
 let dayNumber = todayObj.getDay()
-let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"]
+let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday ", "Thursday", "Friday", "Saturday"]
 let day = daylist[dayNumber]
 toDaySpan.innerHTML = day
 
 // change title when tab changes
 let docTitle = document.title;
-window.addEventListener("blur", () =>{
+window.addEventListener("blur", () => {
     document.title = "ʎuoS uɐlıW";
 })
-window.addEventListener("focus", () =>{
+window.addEventListener("focus", () => {
     document.title = docTitle;
 })
