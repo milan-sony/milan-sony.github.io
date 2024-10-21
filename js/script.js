05d4a92c613ee491fcee008e5e9b483a5268e8b7
Milan Sony <milansonyofficial@gmail.com>
2024-10-21 11:45:31 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -3,8 +3,32 @@ window.onload = function () {
         // preloader
         document.getElementById("loader").style.display = "none";
         document.getElementById("content").style.display = "block";
-    }, 5000); // 3000 milliseconds = 3 seconds
+    }, 1000); // 3000 milliseconds = 3 seconds
 };
+
+const icons = [
+    "./assets/sun.gif",
+    "./assets/moon.gif"
+]
+
+// toggle day and night
+let daynnighticon = document.getElementById('daynnighticon');
+let isDay = true;
+
+daynnighticon.addEventListener('click', ()=>{
+
+    if(isDay == true){
+        daynnighticon.src = icons[0]
+        isDay = false
+    }else{
+        isDay = false
+        daynnighticon.src = icons[1]
+        isDay = true;
+    }
+
+})
+
+
 
 
 // scroll to top btn
@@ -15,7 +39,7 @@ window.addEventListener("scroll", () => {
     } else {
         ScrollToTop.style.display = "none";
     }
-})
+});
 
 // today's day
 let toDaySpan = document.getElementById("today")
